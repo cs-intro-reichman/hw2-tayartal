1be5de7a757d802d378919764480f2195a45c9c5
Tal Tayar <ttayar@google.com>
2024-11-21 16:42:56 +0200

    collatz

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,26 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int counter = 0 ; 
+		
+		for (int i = 1; i <= seed; i++) {
+			int ans = i;
+			while (ans != 1) {
+				if (ans % 2 != 0 ) {
+					ans  = ans * 3 + 1;
+				} else ans /= 2; 
+				if(mode.equals("v")) {
+					System.out.println(ans + " ");
+				}
+				counter ++; 
+			}
+			if(mode.equals("v")) {
+				System.out.println("(" + counter +")\n");
+			}
+		}
+		System.out.println("Every one of the first " + seed + "hailstone sequences reached 1.");
+
 	}
 }
