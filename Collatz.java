0d8dfeb8340a81a3b18029441c7c0bd099df4ff5
Tal Tayar <ttayar@google.com>
2024-11-21 17:56:18 +0200

    do_whie

--- Collatz.java
+++ Collatz.java
@@ -7,18 +7,19 @@ public class Collatz {
 		int counter = 0 ; 
 		
 		for (int i = 1; i <= seed; i++) {
-			int ans = 0 ;
-			//counter = 1 ;
-			while (ans != 1) {
-				ans = i;
-				if (ans % 2 != 0 ) {
-					ans  = ans * 3 + 1;
-				} else ans /= 2; 
-				if(mode.equals("v")) {
-					System.out.print(ans + " ");
-				}
-				counter ++; 
-			}
+            int ans = i;
+			counter = 0 ;
+            do { 
+                if (ans % 2 != 0) {
+                    ans = ans * 3 + 1;
+                } else {
+                    ans /= 2;
+                }
+                if (mode.equals("v")) {
+                    System.out.println(ans + " ");
+                }
+                counter++;
+            } while (ans != 1); 
 
 			if(mode.equals("v")) {
 				System.out.print("(" + counter +")\n");
